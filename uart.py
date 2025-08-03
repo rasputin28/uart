fe9322fbe80e4489707a9e56d2505cabb886fbb1
Joel Suro Villalobos <jsv2858@gmail.com>
2025-08-03 16:21:52 -0600

    firs uart test

---
+++ uart.py
@@ -0,0 +1,23 @@
+import serial
+
+# Initialize serial connection
+ser = serial.Serial("/dev/AMA0", baudrate=115200)
+
+print("UART test started, type something for I/O or simply listen for device")
+
+try:
+    while True:
+        data = input("Send: ")
+        if not data:
+            continue
+        ser.write(data.encode())
+        print("Waiting for echo...")
+        echo = ser.read(len(data))
+        print("Received: ", echo.decode(errors="ignore"))
+except KeyboardInterrupt:
+    print("Exiting (Ctrl+C pressed)")
+except Exception as e:
+    print("Error:", e)
+finally:
+    ser.close()
+
